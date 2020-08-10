66b6a179115ba04cde966c2df55a81ac4b5e5934
Fara <aiypbayev@gmail.com>
2020-08-10 20:39:48 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+        import tabs from './modules/tabs'; 
+        import forms from './modules/forms'; 
+        import timer from './modules/timer'; 
+        import modal from './modules/modal'; 
+        import cards from './modules/cards'; 
+        import slider from './modules/slider'; 
+        import calculator from './modules/calculator';
+        import {openModal} from './modules/modal';
+
+window.addEventListener('DOMContentLoaded', function() {
+    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 300000);
+
+
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    forms('form', modalTimerId);
+    timer('.timer', '2020-09-11');
+    modal('[data-modal]', '.modal', modalTimerId);
+    cards();
+    slider({
+        container: '.offer__slider',
+        nextArrow: '.offer__slider-next',
+        prevArrow: '.offer__slider-prev',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner',
+        slide: '.offer__slide'
+    });
+    calculator();
+});
